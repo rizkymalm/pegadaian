28dd131294a3739af1974eccd985521088f601b6
rizkymalm <rizkymalm@gmail.com>
2020-08-23 21:06:26 +0700

    flexmonster js

--- controllers/report.js
+++ controllers/report.js
@@ -1,16 +1,40 @@
 const moment = require("moment")
 const db = require("../models/db");
+const fs = require("fs")
+const path = require("path")
 exports.getReport = (req,res) => {
     if(req.session.email==undefined){
-        res.redirect("./login")
+        res.redirect("../login")
     }else{
         var login = ({idses: req.session.id, nameses: req.session.name, emailses: req.session.email, subbranch: req.session.subbranch})
-        db.query("SELECT * FROM sub_branch WHERE id_sub_branch=?", login.subbranch, (err,sub_branch)=>{
-            res.render("report", {
-                login: login,
-                moment: moment,
-                subBranch: sub_branch
-            })
+        if(req.query.skenario==undefined){
+            var directoryPath = path.join(__dirname, '../public/rawdatacsv/gadai/')
+            var dir = "gadai"
+        }else{
+            if(req.query.skenario=="gadai"){
+                var dir = "gadai"
+                var directoryPath = path.join(__dirname, '../public/rawdatacsv/gadai/')
+            }else if(req.query.skenario=="telepon"){
+                var dir = "telepon"
+                var directoryPath = path.join(__dirname, '../public/rawdatacsv/telepon/')
+            }else if(req.query.skenario=="pelunasan"){
+                var dir = "pelunasan"
+                var directoryPath = path.join(__dirname, '../public/rawdatacsv/pelunasan/')
+            }
+        }
+        fs.readdir(directoryPath, function(err,files){
+            for(var f=0;f<files.length;f++){
+                var readfilecsv = files[f]
+                db.query("SELECT * FROM sub_branch WHERE id_sub_branch=?", login.subbranch, (err,sub_branch)=>{
+                    res.render("report", {
+                        login: login,
+                        moment: moment,
+                        subBranch: sub_branch,
+                        filecsv: readfilecsv,
+                        dir: dir
+                    })
+                })
+            }
         })
     }
 }
